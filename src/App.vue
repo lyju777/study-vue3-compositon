a7531fb7aacd3a309966decdd4804e7b614f7c30
lyju777 <lyju777@hanmail.net>
2023-11-20 16:54:42 +0900

    project  생성

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <TodoHeader></TodoHeader>
+</template>
+
+<script>
+import TodoHeader from "./components/TodoHeader.vue";
+
+export default {
+  components: { TodoHeader },
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
