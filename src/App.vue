a544d7c1b016033453f7a0171d0f611b10122113
lyju777 <lyju777@hanmail.net>
2023-11-21 12:35:49 +0900

    할 일 관리 앱을 만들면서 배우는 Composition API

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,45 @@
 <template>
   <TodoHeader></TodoHeader>
+  <TodoInput @add="addTodoItem"></TodoInput>
+  <TodoList :todoItems="todoItems" @remove="removeTodoItem"></TodoList>
 </template>
 
 <script>
+import { ref } from "vue";
 import TodoHeader from "./components/TodoHeader.vue";
+import TodoInput from "./components/TodoInput.vue";
+import TodoList from "./components/TodoList.vue";
 
 export default {
-  components: { TodoHeader },
+  components: { TodoHeader, TodoInput, TodoList },
+
   setup() {
-    return {};
+    const todoItems = ref([]);
+
+    function fetchTodos() {
+      const result = [];
+      for (let i = 0; i < localStorage.length; i++) {
+        const todoItem = localStorage.key(i);
+        // items.value.push(todoItem);
+        result.push(todoItem);
+      }
+      return result;
+    }
+
+    todoItems.value = fetchTodos();
+
+    function addTodoItem(todo) {
+      todoItems.value.push(todo);
+      localStorage.setItem(todo, todo);
+    }
+
+    return { todoItems, addTodoItem };
+  },
+  methods: {
+    removeTodoItem(item, index) {
+      this.todoItems.splice(index, 1);
+      localStorage.removeItem(item);
+    },
   },
 };
 </script>
